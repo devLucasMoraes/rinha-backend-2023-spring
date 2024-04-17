0fd6304f55f1ffb94e6f4dd407b9775d7fc1eee9
devLucasMoraes <devlucasmoraes@gmail.com>
2024-04-17 14:48:43 -0300

    Initial commit

---
+++ src/main/java/com/api/rinhabackend2023spring/RinhaBackend2023SpringApplication.java
@@ -0,0 +1,13 @@
+package com.api.rinhabackend2023spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackend2023SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaBackend2023SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rinhabackend2023spring/RinhaBackend2023SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rinhabackend2023spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackend2023SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
